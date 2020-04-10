42a03292c301db8bde74cef6dfc132ab42949ef1
BaileyFirman <bailey.firman@nintex.com>
2020-04-10 20:34:24 +1200

    DB-18: Updated GameScene to initialize game, sprites, and all game behaviors

--- Dropblocks/GameScene.swift
+++ Dropblocks/GameScene.swift
@@ -2,7 +2,7 @@
 //  GameScene.swift
 //  Dropblocks
 //
-//  Created by Bailey Firman on 9/04/20.
+//  Created by Bailey Firman on 4/04/20.
 //  Copyright © 2020 Bailey Firman. All rights reserved.
 //
 
@@ -10,101 +10,101 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
     var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+    var graphs = [String: GKGraph]()
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+    let positioning: PPositioning
+    var userBlocks: PlayerBlocks
+
+    var scoreLabel: InformationLabel
+    var timeLabel: InformationLabel
+    var liveLabel: Lives
+
+    var lastUpdateTime: TimeInterval = 0
+
+    override init(size: CGSize) {
+        self.positioning = Positioning()
+        self.userBlocks = BaseBlocks(positioning: positioning)
+        self.scoreLabel = ScoreLabel(positioning: positioning)
+        self.timeLabel = TimeLabel(positioning: positioning)
+
+        self.liveLabel = Lives(positioning: positioning, maximumLives: 3)
+        super.init(size: size)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.positioning = Positioning()
+        self.userBlocks = BaseBlocks(positioning: positioning)
+        self.scoreLabel = ScoreLabel(positioning: positioning)
+        self.timeLabel = TimeLabel(positioning: positioning)
+        self.liveLabel = Lives(positioning: positioning, maximumLives: 3)
+        super.init(coder: aDecoder)
+    }
+
+    override func sceneDidLoad() {
+    }
+
+    private func initalizeWorld() {
+        backgroundColor = SKColor.white
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+    }
+
+    private func initalizePlayerBlocks() {
+        userBlocks.getBlocks().forEach { (baseBlock) in
+            addChild(baseBlock)
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        addChild(userBlocks.getBaseObscurer())
+    }
+
+    private func initalizeLiveIndicators() {
+        liveLabel.indicators.forEach { (indicator) in
+            addChild(indicator)
         }
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+
+    private func initalizeLabels() {
+        guard let score = scoreLabel as? SKNode, let time = timeLabel as? SKNode else {
+            return
         }
+        addChild(time)
+        addChild(score)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
+    private func incrementGameTime() {
+        _ = timeLabel.increment()
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
+    override func didMove(to view: SKView) {
+        initalizeWorld()
+        initalizePlayerBlocks()
+        initalizeLabels()
+        initalizeLiveIndicators()
+
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(createAndDropFallingBlock),
+                SKAction.wait(forDuration: 0.8),
+                SKAction.run(incrementGameTime)
+            ])
+        ))
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            swapPlayerBlocks(touch: touch)
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
+        if self.lastUpdateTime == 0 {
             self.lastUpdateTime = currentTime
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
+
+        let deltaTime = currentTime - self.lastUpdateTime
         for entity in self.entities {
-            entity.update(deltaTime: dt)
+            entity.update(deltaTime: deltaTime)
         }
-        
         self.lastUpdateTime = currentTime
     }
 }
